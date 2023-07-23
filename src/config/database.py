0a279c77d8aef4d48795f2c2e3b164c0839e96d0
Biongar <mixailis5050@gmail.com>
2023-07-23 14:36:18 +0300

    env переменные подтягиваем теперь из settings

--- src/config/database.py
+++ src/config/database.py
@@ -1,21 +1,24 @@
 import os
 from contextlib import contextmanager
 
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from .settings import DEBUG
-
-load_dotenv()
+from .settings import (
+    DEBUG,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+    POSTGRES_HOST,
+    POSTGRES_DB,
+)
 
 
 DATABASE_URL = "postgresql+psycopg2://{user}:{password}@{host}/{db_name}"
 DATABASE_URL = DATABASE_URL.format(
-    user=os.environ.get("POSTGRES_USER"),
-    password=os.environ.get("POSTGRES_PASSWORD"),
-    host=os.environ.get("POSTGRES_HOST"),
-    db_name=os.environ.get("POSTGRES_DB"),
+    user=POSTGRES_USER,
+    password=POSTGRES_PASSWORD,
+    host=POSTGRES_HOST,
+    db_name=POSTGRES_DB,
 )
 
 engine = create_engine(DATABASE_URL, echo=DEBUG)
--- src/config/settings.py
+++ src/config/settings.py
@@ -7,3 +7,19 @@ load_dotenv()
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = bool(os.environ.get("DEBUG"))
+
+
+# DATABASE settings
+
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+
+
+# SMTP settings
+
+EMAIL_LOGIN = os.environ.get("EMAIL_LOGIN")
+EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+SMTP_HOST = os.environ.get("SMTP_HOST")
+SMTP_PORT = int(os.environ.get("SMTP_PORT"))
